ed9c3f9292d279c6599abee727ac3dd8a453c5d4
Victor <Victor@hwd.com>
2022-02-16 12:11:20 +0800

    first commit

---
+++ src/main/java/com/hwd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hwd.git;
+
+/**
+ * @author 黄伟东/Victor
+ * @date 2022/2/16 12:02
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
